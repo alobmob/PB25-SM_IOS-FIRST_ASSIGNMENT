15248b6e1d3d1784e2088ce48b9193f8011afd50
alobmob <welds-natural1n@icloud.com>
2025-11-11 15:45:12 +0100

    Initial Commit

---
+++ first-assignment/first_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_assignmentApp.swift
+//  first-assignment
+//
+//  Created by Krystian Bombola on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct first_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
